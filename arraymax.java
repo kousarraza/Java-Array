25c7c91cbeb7539c1a99be597f94d49ceb2700d7
Kousar Raza <49365112+kousarraza@users.noreply.github.com>
2020-10-21 13:57:12 +0500

    Add files via upload

---
+++ arraymax.java
@@ -0,0 +1,12 @@
+public class arraymax
+{
+public static void main(String arg[])
+{
+int num[]={12,34,56,78,90};
+int max=num[0];
+for(int i=1; i<num.length;i++)
+if(num[i]>max)
+max=num[i];
+System.out.println("max"+max);
+}
+}
